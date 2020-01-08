4c5fb7b20cae7ce25cb628aaef446794c54406c6
mike seibel <mikes@washington.edu>
2020-01-08 13:25:19 -0800

    prometheus test

---
+++ restclients_core/tests/test_prometheus.py
@@ -0,0 +1,14 @@
+from restclients_core.tests.dao_implementation.test_backend import TDAO
+from unittest import TestCase
+from prometheus_client import generate_latest, REGISTRY
+
+
+class TestPrometheusObservations(TestCase):
+
+    def test_prometheus_observation(self):
+        response = TDAO().getURL('/ok')
+        metrics = generate_latest(REGISTRY)
+        self.assertIn('restclient_request_duration_seconds_bucket{le="0.005",'
+                      'service="backend_test"} 1.0', metrics.decode('utf-8'))
+        self.assertIn('restclient_response_status_code_bucket{le="200.0",'
+                      'service="backend_test"} 1.0', metrics.decode('utf-8'))
