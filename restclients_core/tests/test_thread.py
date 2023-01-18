c926c01ac19a9d17b9506d8327cb517d1c067f56
Jim Laney <jlaney@uw.edu>
2023-01-18 08:57:38 -0800

    close db connections for threads

--- restclients_core/tests/test_thread.py
+++ restclients_core/tests/test_thread.py
@@ -3,23 +3,53 @@
 
 from unittest import TestCase
 from commonconf import settings, override_settings
-from restclients_core.thread import is_django_mysql
+from restclients_core.thread import Thread
+from mock import patch
+
+
+class MockSQLite():
+    vendor = 'sqlite'
+
+
+class MockMySQL():
+    vendor = 'mysql'
 
 
 class TestThread(TestCase):
-    def test_is_django_mysql(self):
-        with override_settings(DATABASES=None):
-            self.assertFalse(is_django_mysql())
+    @patch('restclients_core.thread.db_connection', MockMySQL)
+    def test_use_threading_mysql(self):
+        thread = Thread()
+        self.assertTrue(thread._use_thread)
 
-        with override_settings(DATABASES='mysql'):
-            self.assertFalse(is_django_mysql())
+        with override_settings(RESTCLIENTS_DISABLE_THREADING=True):
+            thread = Thread()
+            self.assertFalse(thread._use_thread)
 
-        with override_settings(
-                DATABASES={'default': {
-                    'ENGINE': 'django.db.backends.sqlite3'}}):
-            self.assertFalse(is_django_mysql())
+        with override_settings(RESTCLIENTS_DISABLE_THREADING=False):
+            thread = Thread()
+            self.assertTrue(thread._use_thread)
 
-        with override_settings(
-                DATABASES={'default': {
-                    'ENGINE': 'django.db.backends.mysql'}}):
-            self.assertTrue(is_django_mysql())
+    @patch('restclients_core.thread.db_connection', MockSQLite)
+    def test_use_threading_sqlite(self):
+        thread = Thread()
+        self.assertFalse(thread._use_thread)
+
+        with override_settings(RESTCLIENTS_DISABLE_THREADING=True):
+            thread = Thread()
+            self.assertFalse(thread._use_thread)
+
+        with override_settings(RESTCLIENTS_DISABLE_THREADING=False):
+            thread = Thread()
+            self.assertFalse(thread._use_thread)
+
+    def test_use_threading_nodb(self):
+        thread = Thread()
+        self.assertFalse(thread._use_thread)
+
+        with override_settings(RESTCLIENTS_USE_THREADING=True):
+            thread = Thread()
+            self.assertTrue(thread._use_thread)
+
+        with override_settings(RESTCLIENTS_USE_THREADING=False):
+            thread = Thread()
+            self.assertFalse(thread._use_thread)
