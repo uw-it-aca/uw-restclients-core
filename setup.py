2a1390fb4eb669e8e90ea3d9a0bb111e1e2cc9ea
Jim Laney <jlaney@uw.edu>
2024-02-03 09:07:27 -0800

    add python version matrix to cicd tests

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='UW-RestClients-Core',
     version=VERSION,
     packages=['restclients_core'],
-    author="UW-IT AXDD",
+    author="UW-IT T&LS",
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=['commonconf',
@@ -39,7 +39,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
     ],
 )
 
